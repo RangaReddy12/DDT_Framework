8443aa3cee9bf77994f15ebe2dde2de431bb8bd5
Qedge <Qedge@DESKTOP-1RLTEAV.Dlink>
2025-04-08 09:13:09 +0530

    Modified values

--- DDT_FrameWork/src/test/java/commonFunction/Demo.java
+++ DDT_FrameWork/src/test/java/commonFunction/Demo.java
@@ -3,14 +3,14 @@ package commonFunction;
 public class Demo {
 public void add()
 {
-	System.out.println("Addition");
+	System.out.println("add");
 }
 public void div()
 {
-	System.out.println("Division");
+	System.out.println("div");
 }
 public void mul()
 {
-	System.out.println("multiplication");
+	System.out.println("mul");
 }
 }
